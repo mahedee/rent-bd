38b5b4ee50463141cd265c6aea7e0c855620a235
Engr Nabi <engrnabi@gmail.com>
2018-05-01 13:28:28 +0600

    update District

--- RentBD-V2/RentBD/Controllers/DistrictController.cs
+++ RentBD-V2/RentBD/Controllers/DistrictController.cs
@@ -8,39 +8,47 @@ using System.Web.Mvc;
 
 namespace RentBD.Controllers
 {
-    public class DivisionController : Controller
+    public class DistrictController : Controller
     {
-        // GET: Division
+        // GET: District
         public ActionResult Index()
         {
-            DivisionRepo divisionRepo = new DivisionRepo();
-            List<Division> divisionList = new List<Division>();
-            divisionList = divisionRepo.GetDivisionInfoAll();
-            return View(divisionList);
+
+            DistrictRepo districtRepro = new DistrictRepo();
+            List<District> districtList = new List<District>();
+            districtList = districtRepro.GetAllDistrictInfo();
+            return View(districtList);
+
+           
         }
 
-        // GET: Division/Details/5
+        // GET: District/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Division/Create
+        // GET: District/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Division/Create
+        // POST: District/Create
         [HttpPost]
-        public ActionResult Create(Division division)
+        public ActionResult Create(District district)
         {
             try
             {
+                DistrictRepo districtrepo = new DistrictRepo();
+               string msg= districtrepo.insertDistrict(district);
+
+
+
+
+
                 // TODO: Add insert logic here
 
-                DivisionRepo divisionRepo = new DivisionRepo();
-                string msg = divisionRepo.InsertDivision(division);
                 return RedirectToAction("Index");
             }
             catch
@@ -49,26 +57,24 @@ namespace RentBD.Controllers
             }
         }
 
-        // GET: Division/Edit/5
+        // GET: District/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Division/Edit/5
+        // POST: District/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id,District district)
+        public ActionResult Edit(int id, District district)
         {
             try
             {
-               
-                
-                ///
-                
-                
-                
-                
                 // TODO: Add update logic here
+
+                DistrictRepo districtRepro = new DistrictRepo();
+                districtRepro.updateDistrictinfo(district);
+
+
 
                 return RedirectToAction("Index");
             }
@@ -78,13 +84,13 @@ namespace RentBD.Controllers
             }
         }
 
-        // GET: Division/Delete/5
+        // GET: District/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Division/Delete/5
+        // POST: District/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
